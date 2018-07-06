f1b8f1932d91dc93f162dd77fba521de8d7ce030
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-06 09:53:19 -0400

    Add implementation from pairing session with Nick
    Add dependencies for spec and basic definitions for Game of Life.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject gol "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "The Game of Life implementated in Clojure."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [expound "0.7.1"]
+                 [org.clojure/test.check "0.9.0"]])
--- src/gol/core.clj
+++ src/gol/core.clj
@@ -1,6 +1,48 @@
-(ns gol.core)
+(ns gol.core
+  (:require [clojure.spec.alpha :as spec]
+            [expound.alpha :as expound]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def empty-board [[0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]
+                  [0 0 0 0 0 0 0 0 0 0]])
+
+(spec/def ::coords (spec/cat :x (set (range 0 10)) :y (set (range 0 10))))
+(spec/def ::cell #{0 1})
+(spec/def ::row (spec/coll-of ::cell :kind vector? :count 10))
+(spec/def ::board (spec/coll-of ::row :count 10))
+
+(spec/fdef on-board?
+  :args (spec/cat :coords ::coords :board ::board)
+  :ret boolean?)
+(defn on-board?
+  ""
+  [x y board]
+  (let [height (count board)
+        width (count (first board))]
+    (and (>= x 0) (>= y 0) (< x width) (< y height))))
+
+(defn neighbors
+  ""
+  [x y]
+  []
+  )
+
+(comment
+
+  (set! spec/*explain-out* expound/printer)
+  ;; Put your cursor on start of the line (after the above sexp) and type `SPC m e e`
+
+
+  ;; Any live cell with two or three live neighbors lives on to the next generation.
+
+  ;; Any live cell with more than three live neighbors dies, as if by overpopulation.
+
+  ;; Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+  )
